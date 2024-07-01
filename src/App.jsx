378ea2dd6d8250868399ee79222477996f2243d3
Luis <luis.subiabre@gmail.com>
2024-07-01 11:34:43 -0300

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { EquipoDirectivo } from './components/EquipoDirectivo';
+
 import Hero from './components/Hero';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import { EquipoDirectivo } from './components/EquipoDirectivo';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename="/leumag-react">
-      <Navbar />
-      <Hero />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Equipo" element={<EquipoDirectivo />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+ 
+        <Navbar />
+        <Hero />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/EquipoDirectivo' element={<EquipoDirectivo />} />
+       
+        </Routes>
+      </>
   );
 }
 
